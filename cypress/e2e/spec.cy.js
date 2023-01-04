1244f24786168b7f4a9da187fe954875d87fa4c7
Scott <scott.bickish@intimetec.com>
2023-01-04 09:17:33 -0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,61 @@
+
+describe('Basic crud method api tests', () => {
+
+  it('GET', () => {
+    cy.request('/api/users?page=2').then((response) => {
+      expect(response).to.have.property('status', 200);
+      expect(response.status).equal(200);
+      for (let i = 0; i > response.body.data.length; i++) {
+        if ([i].first_name === 'Rachel')
+          break;
+      }
+      expect(response.body.data[0].first_name).equal('Michael');
+    });
+  });
+
+  it('POST', () => {
+    let user = {
+      "name": "Scott",
+      "job": "Software Engineer SDET"
+    }
+    cy.request('POST', '/api/users', user).then((response) => {
+      expect(response.status).equal(201);
+      expect(response.body.name).equal(user.name);
+      expect(response.body.job).equal(user.job);
+    });
+  });
+
+  it('PUT', () => {
+    let user1 = {
+      "name": "Scott",
+      "job": "AccountAdmin"
+    }
+    cy.request('PUT', '/api/users/2', user1).then((response) => {
+      expect(response.status).equal(200);
+      expect(response.body.name).equal(user1.name);
+      expect(response.body.job).equal(user1.job);
+    });
+  });
+
+  it('DELETE', () => {
+    cy.request('DELETE', '/api/users/2').then((response) => {
+      expect(response.status).equal(204);
+    });
+  });
+
+  it('PATCH', () => {
+    let user2 = {
+      "job": "AccountAdmin"
+    };
+    cy.request('PATCH', '/api/users/2', user2).then((response) => {
+      expect(response.status).equal(200);
+      expect(response.body.job).equal(user2.job);
+
+    });
+  });
+
+  it.only('login using custom command', () => {
+    cy.visit('https://admin-demo.nopcommerce.com/login');
+    cy.Login(Cypress.env('useremail'), Cypress.env('userpassword'));
+  })
+}); 
